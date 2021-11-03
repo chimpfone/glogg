d8823523c9a0cc0910f11f09d687c90f6ba67f38
chimpfone <chimpfone@protonmail.com>
2021-11-03 08:28:59 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import glogg from './glogg.JPG';
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={glogg} className="Glogg-logo" alt="glogg" />
+        const element = <h1 style={{ color: 'black' }}>Glogg.</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
