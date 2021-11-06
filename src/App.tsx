6a7cadc47cfb5efb0e17e7dad366cdda0aab89e6
chimpfone <chimpfone@protonmail.com>
2021-11-06 12:54:07 +0000

    typescript change

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import glogg from './glogg.JPG';
 import './App.css';
+import glogg from './glogg.png';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={glogg} className="Glogg-logo" alt="glogg" />
-        const element = <h1 style={{ color: 'black' }}>Glogg.</h1>
+        <h1 style={{ color: 'black' }}>Glogg.</h1>
       </header>
     </div>
   );
